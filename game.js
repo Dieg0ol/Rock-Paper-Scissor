1c3dd51203593cbb7d0dbc441c103b3b239b1998
Diego Alves <diegocaboclo@hotmail.com>
2023-03-19 22:40:37 -0300

    playRound completed

--- game.js
+++ game.js
@@ -1,10 +1,34 @@
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissor"];
     let computerChoice = Math.floor(Math.random() * choices.length);
-    console.log(choices[computerChoice]); 
-    
-    
+    let test = choices[computerChoice];
+    //console.log(choices[computerChoice]); 
+    return test;
+
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Make your choice by Rock, Paper and Scissor.").toUpperCase();
+    if (playerSelection !== "ROCK" && playerSelection !== "Paper" && playerSelection !== "SCISSORS") {
+        return ("its not work try again");
     }
-    
-    getComputerChoice();
-    
+
+
+    computerSelection = getComputerChoice().toUpperCase();
+    if (playerSelection == "ROCK" && computerSelection == "ROCK") {
+        console.log("its draw");
+    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+        return ("you win");
+    } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+        return ("you win");
+    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+        return ("you win");
+    } else {
+        return ("you lose bro");
+
+    }
+
+}
+playRound();
